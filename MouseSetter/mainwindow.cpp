8d9d5ca0bcc88b5461567fb59ed35bdae0df7f28
codec-abc <viot.camille@outlook.com>
2013-04-13 19:01:56 +0200

    retrieving mouse info

--- MouseSetter/mainwindow.cpp
+++ MouseSetter/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "windows.h"
+#include <iostream>
+#include "ui_dialogMouseInformation.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,35 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//        SystemParametersInfo(SPI_SETMOUSE,0,aMouseInfo,SPIF_SENDCHANGE);
+
+void MainWindow::on_MouseInfoButton_clicked()
+{
+    BOOL fResult1;
+    BOOL fResult2;
+    int aMouseInfo[3];
+    int bMouseInfo = -1;
+    fResult1 = SystemParametersInfo(SPI_GETMOUSE, 0,&aMouseInfo,0);
+    fResult2 = SystemParametersInfo(SPI_GETMOUSESPEED,0,&bMouseInfo,0);
+    std::cout << "push button clicked" << std::endl;
+    QDialog* dialogMouseInformation = new QDialog(0,0);
+    Ui::Dialog dialogMouseInformationUi;
+
+    QString string1 = QString::number(aMouseInfo[0]);
+    QString string2 = QString::number(aMouseInfo[1]);
+    QString string3 = QString::number(aMouseInfo[2]);
+    QString string4 = QString::number(bMouseInfo);
+
+    dialogMouseInformationUi.setupUi(dialogMouseInformation);
+
+    dialogMouseInformationUi.mouse_threshold_0_label->setText(string1);
+    dialogMouseInformationUi.mouse_threshold_1_label->setText(string2);
+    dialogMouseInformationUi.mouse_acceleration_label->setText(string3);
+    dialogMouseInformationUi.mouse_speed_label->setText(string4);
+
+   // dialogMouseInformation->layout()->setSizeConstraint( QLayout::SetFixedSize );
+    dialogMouseInformation->show();
+
 }
--- MouseSetter/mainwindow.h
+++ MouseSetter/mainwindow.h
@@ -15,8 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_MouseInfoButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString string1;
 };
 
 #endif // MAINWINDOW_H
